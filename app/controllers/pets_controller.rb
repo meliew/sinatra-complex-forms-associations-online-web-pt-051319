13cb81c49238f5d5d91a074d5d19e0cf55d8bbe2
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-24 21:51:02 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,7 +14,7 @@ class PetsController < ApplicationController
     if !params["owner"]["name"].empty?
       @pet.owner << Owner.create(name: params["owner"]["name"])
     end
-    
+
 
     redirect "pets/#{@pet.id}"
   end
